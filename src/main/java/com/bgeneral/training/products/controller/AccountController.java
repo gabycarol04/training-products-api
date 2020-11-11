d1d307598a5693a8d91246ea8f10a8fe68fd57be
Carlos Icaza <icazacarlos@gmail.com>
2020-11-10 23:38:01 -0500

    Se agrega el método getAccounts al REST Controller

--- src/main/java/com/bgeneral/training/products/controller/AccountController.java
+++ src/main/java/com/bgeneral/training/products/controller/AccountController.java
@@ -1,7 +1,11 @@
 package com.bgeneral.training.products.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bgeneral.training.products.model.Account;
 
 @RestController
 public class AccountController {
@@ -10,6 +14,47 @@ public class AccountController {
 	public String test() {
 
 		return "Siempre Listos 2020";
+	}
+
+	@GetMapping(value = "/accounts")
+	public ArrayList<Account> getAccounts() {
+
+		// Crear account1
+
+		Account account1 = new Account();
+		account1.setId("1001");
+		account1.setName("Tu primera Cuenta de Ahorros");
+		account1.setDescription("100% digital");
+
+		ArrayList<String> features1 = new ArrayList<>(); // Java 7 no hace falta colocar el tipo
+
+		features1.add("Ábrela en el app solo con tu cédula");
+		features1.add("Nacionales y extranjeros con cédula E");
+
+		account1.setFeatures(features1);
+
+		// Crear account2
+
+		Account account2 = new Account("1002", "Cuenta Mis Ahorros", "Desde pequeños aprendamos el valor de ahorrar",
+				new ArrayList<>());
+
+		// ArrayList<String> features2 = new ArrayList<>(); // Java 7 no hace falta
+		// colocar el tipo
+		ArrayList<String> features2 = account2.getFeatures();
+
+		features2.add("Exclusiva para menores de edad");
+		features2.add("Atractivas tasas de interés");
+
+		// account2.setFeatures(features2);
+
+		// Agregar las cuentas a la lista
+
+		ArrayList<Account> accounts = new ArrayList<Account>();
+
+		accounts.add(account1);
+		accounts.add(account2);
+
+		return accounts;
 	}
 
 }
--- src/main/java/com/bgeneral/training/products/model/Account.java
+++ src/main/java/com/bgeneral/training/products/model/Account.java
@@ -2,20 +2,25 @@ package com.bgeneral.training.products.model;
 
 import java.util.ArrayList;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Account {
 
 	private String id;
-	
+
 	private String name;
-	
+
 	private String description;
 
-	private ArrayList<Integer> features;	// TODO: programar a la interfaz y no a la clase concreta. cambiar a List.
+	private ArrayList<String> features; // TODO: programar a la interfaz y no a la clase concreta. cambiar a List.
+
 }
